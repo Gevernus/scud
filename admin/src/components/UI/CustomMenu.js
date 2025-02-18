f36ed83795f62a5f7918050718f64f9166094fb8
ItalianecXXX <anatoly.bezrukoff@ya.ru>
2025-02-19 00:49:30 +0700

    Fix Width front, add tooltip menu  and users permission popap

--- admin/src/components/UI/CustomMenu.js
+++ admin/src/components/UI/CustomMenu.js
@@ -1,129 +1,158 @@
 import { useState } from 'react';
 import {
-    List,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Collapse,
-    Box,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Collapse,
+  Box,
+  Tooltip,
 } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { FaTrashCan, FaUsers, FaUserPlus } from 'react-icons/fa6';
 import { MdOutlineEventNote, MdExpandLess, MdExpandMore } from 'react-icons/md';
 import { FaServer, FaHome } from 'react-icons/fa';
-import { GiTeamIdea } from 'react-icons/gi'; 
+import { GiTeamIdea } from 'react-icons/gi';
 
 const CustomMenu = ({ open }) => {
-    const [openTrash, setOpenTrash] = useState(false); // Basket opening control
-    
-    return (
-        <List component="nav">
-            <ListItemButton component={Link} to="/">
-                <ListItemIcon><FaHome /></ListItemIcon>
-                {open && <ListItemText primary="Главная" />}
+  const [openTrash, setOpenTrash] = useState(false); // Basket opening control
+
+  return (
+    <List component="nav">
+      <Tooltip title="Главная" placement="right" disableHoverListener={open}>
+        <ListItemButton component={Link} to="/">
+          <ListItemIcon>
+            <FaHome />
+          </ListItemIcon>
+          {open && <ListItemText primary="Главная" />}
+        </ListItemButton>
+      </Tooltip>
+
+      <Tooltip title="Пользователи" placement="right" disableHoverListener={open}>
+        <ListItemButton component={Link} to="/users">
+          <ListItemIcon>
+            <FaUsers />
+          </ListItemIcon>
+          <ListItemText primary="Пользователи" />
+        </ListItemButton>
+      </Tooltip>
+
+      <Tooltip title="Журнал событий" placement="right" disableHoverListener={open}>
+        <ListItemButton component={Link} to="/events">
+          <ListItemIcon>
+            <MdOutlineEventNote />
+          </ListItemIcon>
+          <ListItemText primary="Журнал событий" />
+        </ListItemButton>
+      </Tooltip>
+
+      <Tooltip title="Станции" placement="right" disableHoverListener={open}>
+        <ListItemButton component={Link} to="/stations">
+          <ListItemIcon>
+            <FaServer />
+          </ListItemIcon>
+          <ListItemText primary="Станции" />
+        </ListItemButton>
+      </Tooltip>
+
+      <Tooltip title="Контрагенты" placement="right" disableHoverListener={open}>
+        <ListItemButton component={Link} to="/counterparts">
+          <ListItemIcon>
+            <GiTeamIdea />
+          </ListItemIcon>
+          <ListItemText primary="Контрагенты" />
+        </ListItemButton>
+      </Tooltip>
+
+      <Tooltip title="Регистрация" placement="right" disableHoverListener={open}>
+        <ListItemButton component={Link} to="/registration">
+          <ListItemIcon>
+            <FaUserPlus />
+          </ListItemIcon>
+          <ListItemText primary="Регистрация" />
+        </ListItemButton>
+      </Tooltip>
+
+      {/* Trash - Nested menu */}
+      <Tooltip title="Корзина" placement="right" disableHoverListener={open}>
+        <ListItemButton onClick={() => setOpenTrash(!openTrash)}>
+          <ListItemIcon>
+            <FaTrashCan />
+          </ListItemIcon>
+          <ListItemText primary="Корзина" />
+          {openTrash ? <MdExpandLess /> : <MdExpandMore />}
+        </ListItemButton>
+      </Tooltip>
+
+      <Collapse in={openTrash} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <Tooltip
+            title="Корзина пользователей"
+            placement="right"
+            disableHoverListener={open}
+          >
+            <ListItemButton component={Link} to="/usersTrash" sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <FaTrashCan />
+              </ListItemIcon>
+              <Box>
+                <ListItemText primary="Корзина пользователей" />
+              </Box>
             </ListItemButton>
-            
-            <ListItemButton component={Link} to="/users">
-                <ListItemIcon>
-                    <FaUsers />
-                </ListItemIcon>
-                <ListItemText primary="Пользователи" />
+          </Tooltip>
+
+          <Tooltip
+            title="Корзина событий"
+            placement="right"
+            disableHoverListener={open}
+          >
+            <ListItemButton component={Link} to="/eventsTrash" sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <FaTrashCan />
+              </ListItemIcon>
+              <Box>
+                <ListItemText primary="Корзина событий" />
+              </Box>
             </ListItemButton>
+          </Tooltip>
 
-            <ListItemButton component={Link} to="/events">
-                <ListItemIcon>
-                    <MdOutlineEventNote />
-                </ListItemIcon>
-                <ListItemText primary="Журнал событий" />
+          <Tooltip
+            title="Корзина Станций"
+            placement="right"
+            disableHoverListener={open}
+          >
+            <ListItemButton component={Link} to="/stationsTrash" sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <FaTrashCan />
+              </ListItemIcon>
+              <Box>
+                <ListItemText primary="Корзина Stations" />
+              </Box>
             </ListItemButton>
+          </Tooltip>
 
-            <ListItemButton component={Link} to="/stations">
-                <ListItemIcon>
-                    <FaServer />
-                </ListItemIcon>
-                <ListItemText primary="Станции" />
+          <Tooltip
+            title="Корзина Контрагенты"
+            placement="right"
+            disableHoverListener={open}
+          >
+            <ListItemButton
+              component={Link}
+              to="/counterpartyTrash"
+              sx={{ pl: 4 }}
+            >
+              <ListItemIcon>
+                <FaTrashCan />
+              </ListItemIcon>
+              <Box>
+                <ListItemText primary="Корзина Контрагенты" />
+              </Box>
             </ListItemButton>
-
-            <ListItemButton component={Link} to="/counterparts">
-                <ListItemIcon>
-                    <GiTeamIdea />
-                </ListItemIcon>
-                <ListItemText primary="Контрагенты" />
-            </ListItemButton>
-
-            <ListItemButton component={Link} to="/registration">
-                <ListItemIcon>
-                    <FaUserPlus />
-                </ListItemIcon>
-                <ListItemText primary="Регистрация" />
-            </ListItemButton>
-
-            {/* Trash - Nested menu */}
-            <ListItemButton onClick={() => setOpenTrash(!openTrash)}>
-                <ListItemIcon>
-                    <FaTrashCan />
-                </ListItemIcon>
-                <ListItemText primary="Корзина" />
-                {openTrash ? <MdExpandLess /> : <MdExpandMore />}
-            </ListItemButton>
-
-            <Collapse in={openTrash} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    <ListItemButton
-                        component={Link}
-                        to="/usersTrash"
-                        sx={{ pl: 4 }}
-                    >
-                        <ListItemIcon>
-                            <FaTrashCan />
-                        </ListItemIcon>
-                        <Box >
-                            <ListItemText primary="Корзина пользователей" />
-                        </Box>
-                    </ListItemButton>
-
-                    <ListItemButton
-                        component={Link}
-                        to="/eventsTrash"
-                        sx={{ pl: 4 }}
-                    >
-                        <ListItemIcon>
-                            <FaTrashCan />
-                        </ListItemIcon>
-                        <Box>
-                            <ListItemText primary="Корзина событий" />
-                        </Box>
-                    </ListItemButton>
-
-                    <ListItemButton
-                        component={Link}
-                        to="/stationsTrash"
-                        sx={{ pl: 4 }}
-                    >
-                        <ListItemIcon>
-                            <FaTrashCan />
-                        </ListItemIcon>
-                        <Box>
-                            <ListItemText primary="Корзина Stations" />
-                        </Box>
-                    </ListItemButton>
-
-                    <ListItemButton
-                        component={Link}
-                        to="/counterpartyTrash"
-                        sx={{ pl: 4 }}
-                    >
-                        <ListItemIcon>
-                            <FaTrashCan />
-                        </ListItemIcon>
-                        <Box>
-                            <ListItemText primary="Корзина Контрагенты" />
-                        </Box>
-                    </ListItemButton>
-                </List>
-            </Collapse>
+          </Tooltip>
         </List>
-    );
+      </Collapse>
+    </List>
+  );
 };
 
 export default CustomMenu;
--- admin/src/components/UI/PermissionsField.js
+++ admin/src/components/UI/PermissionsField.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRecordContext } from 'react-admin';
 import { PERMISSIONS_MODULES, checkPermission } from '../../permissions';
+import { Popover, Box, Button } from '@mui/material';
 
 const PermissionsField = ({ source }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
   const record = useRecordContext();
   if (!record) return null;
 
@@ -23,16 +26,48 @@ const PermissionsField = ({ source }) => {
     return <span>Нет прав</span>;
   }
 
+  const handleMouseEnter = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMouseLeave = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
   return (
     <div>
-      {Object.entries(modulePermissions).map(([module, actions]) => (
-        <div key={module}>
-          <strong>{module}</strong>
-          {actions.map((action) => (
-            <span key={action}> - {action}</span>
+      <Button
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        size="small"
+      >
+        Подробнее
+      </Button>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleMouseLeave}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        disableEnforceFocus // Disaches a forced focus
+        disableAutoFocus // Disaches the autofocus
+        sx={{ pointerEvents: 'none' }} // Removes a delay in hiding
+      >
+        <Box sx={{ p: 2, maxWidth: 450 }}>
+          {Object.entries(modulePermissions).map(([module, actions]) => (
+            <div key={module}>
+              <strong>{module}:</strong> {actions.join(', ')}
+            </div>
           ))}
-        </div>
-      ))}
+        </Box>
+      </Popover>
     </div>
   );
 };
