fd38c19cb872f972fbd2f5f1d27a539d7af98769
ItalianecXXX <anatoly.bezrukoff@ya.ru>
2025-02-20 01:20:39 +0700

    Fix permission bekend

--- admin/src/components/UI/RestoreButton.js
+++ admin/src/components/UI/RestoreButton.js
@@ -1,7 +1,8 @@
-
 import { Button, useNotify, useRefresh, useRecordContext } from 'react-admin';
+import { useUser } from '../../context/UserContext';
 
 const RestoreButton = ({ resource }) => {
+    const { user } = useUser();
     // Get the record via useRecordContext
     const record = useRecordContext();
     const notify = useNotify();
@@ -14,12 +15,19 @@ const RestoreButton = ({ resource }) => {
         const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/admin'
         const url = `${apiUrl}/trash/${resource}/${record.id}/restore`;
         try {
+            // Формируем заголовки запроса, добавляя telegramId из контекста
+            const headers = {
+                'Content-Type': 'application/json',
+            };
+
+            if (user?.telegramId) {
+                headers['X-Telegram-ID'] = user.telegramId;
+            }
             const response = await fetch(url, {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
+                headers,
             });
+            
             if (!response.ok) {
                 const errorData = await response.json();
                 throw new Error(errorData.error || 'Ошибка восстановления');
